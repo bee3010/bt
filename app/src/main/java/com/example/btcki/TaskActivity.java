6afbe57b48572d3541b05c017b93d09d5b6efa0d
NguyenVanThang <nguyenvanthang30102004@gmail.com>
2024-11-29 08:23:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/btcki/TaskActivity.java
@@ -0,0 +1,17 @@
+package com.example.btcki;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+
+    }
+
+}
